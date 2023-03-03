d037e9789bf1c3fd8267bebb6d19eae86ec1911f
elamirkad <elamirkad@gmail.com>
2023-03-03 07:42:22 +0600

    Initialized new User microservice on Django

--- product/products/urls.py
+++ product/products/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import CarViewSet, CarImageViewSet, FeatureViewSet, CarFeatureViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Product API",
+        default_version="v1",
+        description="API documentation for Product Microservice",
+        contact=openapi.Contact(email="elamirkad@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[],
+)
 
 
 router = routers.DefaultRouter()
@@ -9,5 +24,12 @@ router.register(r"car-images", CarImageViewSet, basename="car-images")
 router.register(r"features", FeatureViewSet, basename="features")
 router.register(r"car-features", CarFeatureViewSet, basename="car-features")
 
-urlpatterns = []
-urlpatterns += router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
