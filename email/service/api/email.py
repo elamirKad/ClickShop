744ad5eacb20b34e7408d98ce336c6399c5cd217
elamirkad <elamirkad@gmail.com>
2023-03-05 11:19:29 +0600

    email: send_email_async & send_email_background functional

--- email/service/api/email.py
+++ email/service/api/email.py
@@ -1,19 +1,59 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+import os
+from fastapi import APIRouter, BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
 router = APIRouter()
 
 
-class Email(BaseModel):
-    email: str
-    subject: str
-    message: str
+class Envs:
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_FROM = os.getenv("MAIL_FROM")
+    MAIL_PORT = int(os.getenv("MAIL_PORT"))
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_FROM_NAME = os.getenv("MAIN_FROM_NAME")
 
 
-@router.post("/email")
-async def send_email(email: Email):
-    return {"message": "Email sent successfully"}
+conf = ConnectionConfig(
+    MAIL_USERNAME=Envs.MAIL_USERNAME,
+    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
+    MAIL_FROM=Envs.MAIL_FROM,
+    MAIL_PORT=Envs.MAIL_PORT,
+    MAIL_SERVER=Envs.MAIL_SERVER,
+    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+    TEMPLATE_FOLDER="./templates",
+)
+
+
+async def send_email_async(subject: str, email_to: str, template: str, body: dict):
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        body=body,
+        subtype="html",
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message, template_name=template)
+
+
+def send_email_background(
+    background_tasks: BackgroundTasks,
+    subject: str,
+    email_to: str,
+    template: str,
+    body: dict,
+):
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        body=body,
+        subtype="html",
+    )
+    fm = FastMail(conf)
+    background_tasks.add_task(fm.send_message, message, template_name=template)
