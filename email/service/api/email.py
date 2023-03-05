07e0151c5a31730df0e7b2e3b0fdca3c9d10d702
elamirkad <elamirkad@gmail.com>
2023-03-05 12:03:21 +0600

    email: aio_pika rabbitmq message consumer

--- email/service/api/email.py
+++ email/service/api/email.py
@@ -1,10 +1,52 @@
 import os
+import aio_pika
+import asyncio
 from fastapi import APIRouter, BackgroundTasks
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from dotenv import load_dotenv
 
 load_dotenv()
 router = APIRouter()
+
+
+async def consume(loop):
+    # Create a connection to RabbitMQ
+    connection = await aio_pika.connect_robust(
+        "amqp://guest:guest@localhost/", loop=loop
+    )
+
+    # Create a channel
+    channel = await connection.channel()
+
+    # Declare the queue
+    queue = await channel.declare_queue("my_queue")
+
+    # Start consuming messages
+    await queue.consume(message_handler)
+
+
+# Define the message handler function
+async def message_handler(message: aio_pika.IncomingMessage):
+    async with message.process():
+        payload = message.body.decode()
+        subject, email_to, template, body = payload.split("|")
+        await send_email_async(subject, email_to, template, body)
+
+
+@router.on_event("startup")
+async def startup_event():
+    # Start the RabbitMQ consumer
+    loop = asyncio.get_event_loop()
+    await consume(loop)
+    print("Started consuming messages...")
+
+
+@router.on_event("shutdown")
+async def shutdown_event():
+    # Stop the RabbitMQ consumer
+    loop = asyncio.get_event_loop()
+    await loop.shutdown_asyncgens()
+    print("Stopped consuming messages...")
 
 
 class Envs:
