26689d6d9b2ebd955912079dff09e440f5e6a3bc
elamirkad <elamirkad@gmail.com>
2023-03-06 12:37:52 +0600

    feat(swagger): Introduction of swagger to user microservice

--- user/auth_user/urls.py
+++ user/auth_user/urls.py
@@ -1,5 +1,20 @@
 from django.urls import path, include
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import RegisterView, LoginView, UserView, LogoutView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="User API",
+        default_version="v1",
+        description="API documentation for User Microservice",
+        contact=openapi.Contact(email="elamirkad@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[],
+)
 
 
 urlpatterns = [
@@ -7,4 +22,10 @@ urlpatterns = [
     path("login", LoginView.as_view()),
     path("user", UserView.as_view()),
     path("logout", LogoutView.as_view()),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
--- user/user/settings.py
+++ user/user/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "auth_user",
     "corsheaders",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
